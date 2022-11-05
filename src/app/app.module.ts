d5b7129ac8a6b4f85e0059a2f77c95b19a790095
alejandrotoro18 <43111688+alejandrotoro18@users.noreply.github.com>
2022-11-05 08:23:27 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SharedModule } from '../../shared/shared/shared.module';
+import { GifsModule } from '../../gifs/gifs/gifs.module';
 
 
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,SharedModule ],
+  imports:      [ BrowserModule, FormsModule,SharedModule,GifsModule ],
   declarations: [ AppComponent, HelloComponent],
   bootstrap:    [ AppComponent ]
 })
